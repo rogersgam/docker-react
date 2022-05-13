e8927559a9fba70a595230f76c13390ce6c3dc58
rogersgam <56659897+rogersgam@users.noreply.github.com>
2022-05-13 14:07:00 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New feature branch: Rogerio de Sousa Gameiro
+          New feature branch: Rogerio Gameiro
         </p>
         <a
           className="App-link"
